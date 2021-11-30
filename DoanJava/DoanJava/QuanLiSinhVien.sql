f31c06042879627d62ef9f09b74d52206a4dcc62
phongtran0803 <95273211+phongtran0803@users.noreply.github.com>
2021-11-30 15:05:10 +0700

    upload

---
+++ DoanJava/DoanJava/QuanLiSinhVien.sql
@@ -0,0 +1,31 @@
+﻿--A. TẠO CƠ SỞ DỮ LIỆU
+Create Database QuanLiSinhVien;
+Use QuanLiSinhVien;
+--B. TẠO BẢNG
+CREATE TABLE Khoa(
+MaKhoa NVARCHAR(5) PRIMARY KEY NOT NULL,
+TenKhoa NVARCHAR(30)
+)
+
+Create table SinhVien(
+MaSV nvarchar(30),
+HoTen nvarchar(50),
+MaKhoa NVARCHAR(5),
+NgaySinh nvarchar(50),
+GioiTinh bit,
+DiaChi nvarchar(50),
+constraint PK_SINHVIEN primary key (MaSV),
+CONSTRAINT fk_SinhVien FOREIGN KEY(MaKhoa) REFERENCES dbo.Khoa(MaKhoa)
+)
+
+Create table BANGDIEM
+(
+MaSV nvarchar(30),
+DiemJava float,
+DiemSQL float,
+DiemTB float,
+constraint PK_BANGDIEM primary key (MaSV),
+Constraint FK_BANGDIEM_SINHVIEN foreign key (MaSV) references SINHVIEN(MASV),
+)
+Select * From Khoa
+Select * From SinhVien
---
+++ DoanJava/DoanJava/QuanLiSinhVien/src/QuanLiSinhVien/Jrame/Connect.java
@@ -0,0 +1,32 @@
+package QuanLiSinhVien.Jrame;
+
+import Connect.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class Connect {
+
+        Connection con;
+    public Connection getConnection() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=QuanLiSinhVien;user=sa;password=123456");
+            if (con == null) {
+                JOptionPane.showMessageDialog(null, "Không kết nối được Server");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Lỗi" + ex);
+        }
+        return con;
+    }
+    public static void main(String[] args) {
+   Connect a = new Connect();
+    
+}
+}
+
+
